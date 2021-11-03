43714b25b5f84b890389ea382cbabe84a08d6b1e
Verona <verona.m@outlook.de>
2021-11-03 20:44:54 +0100

    added get Order by Id in Controller and Router

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -36,4 +36,17 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 })
 
-export{addOrderItems}
+//  get Order by id
+// GET /api/orders/:id 
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate('user', 'name email')
+
+  if(order){
+    res.json(order)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+
+export{addOrderItems, getOrderById}
