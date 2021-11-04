66efe0b79f577d05d5116bb7beaa038615ddd712
Verona <verona.m@outlook.de>
2021-11-04 23:43:48 +0100

    added upload Images with multer

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -8,6 +8,7 @@ const getProducts = asyncHandler(async (req, res) => {
     res.json(products)
 })
 
+//get product by id
 const getProductById = asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
 
@@ -19,7 +20,77 @@ const getProductById = asyncHandler(async (req, res) => {
     }
 })
 
+//delete a product
+// DELETE /api/products/:id <- admin
+const deleteProduct = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if(product){
+        await product.remove()
+        res.json({message: 'Product removed'})
+    } else {
+        res.status(404)
+        throw new Error('Product not found')
+    }
+})
+
+// create a product
+// POST /api/products <-admin
+const createProduct = asyncHandler(async (req, res) => {
+    const product = new Product({
+        name: 'Sample name',
+        price: 0,
+        user: req.user._id,
+        image: '/images/sample.jpg',
+        artist: 'Sample Artist',
+        year: 0,
+        technique: 'Sample Technique',
+        description: 'Sample Description',
+        countInStock: 0
+    })
+
+    const createdProduct = await product.save()
+    res.status(201).json(createdProduct)
+})
+
+// update a product
+// PUT /api/products/:id <-admin
+const updateProduct = asyncHandler(async (req, res) => {
+    const {
+        name, 
+        price, 
+        description, 
+        image,
+        year,
+        artist,
+        technique,
+        countInStock
+    } = req.body
+
+    const product = await Product.findById(req.params.id)
+
+    if(product) {
+        product.name = name
+        product.price = price,
+        product.description = description
+        product.image = image
+        product.year = year
+        product.artist = artist
+        product.technique = technique
+        product.countInStock = countInStock
+
+        const updatedProduct = await product.save()
+        res.json(updatedProduct)
+    } else {
+        res.status(404)
+        throw new Error('Product not found')
+    }
+    
+})
 export {
     getProducts,
-    getProductById
+    getProductById,
+    deleteProduct,
+    createProduct,
+    updateProduct
 }
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,15 +1,22 @@
 import express from 'express'
 const router = express.Router()
-import {getProducts,getProductById } from '../controllers/productControllers.js'
+import {getProducts,getProductById, deleteProduct, createProduct, updateProduct } from '../controllers/productControllers.js'
+import {protect, admin} from '../middleware/authMiddleware.js'
 
 
 // Fetch all products
 //GET /api/products
-router.route('/').get(getProducts)
+// create product <- admin
+router.route('/')
+    .get(getProducts)
+    .post(protect, admin, createProduct)
 
 // Fetch single product
 // GET /api/products/:id
-router.route('/:id').get(getProductById)
+router.route('/:id')
+    .get(getProductById)
+    .delete(protect,admin, deleteProduct)
+    .put(protect, admin, updateProduct)
 
 export default router
 
